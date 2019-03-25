b3d74edbaba0ab513942e7914bcd4dd7d1f8b160
Lakshya Sharma <47375693+Lakshya31@users.noreply.github.com>
2019-03-25 18:14:40 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a = 52;
+int b = 66a;
+int c=1.2;
+float x = 22.2;
+float y = 24;
+float z = 12..3;
+}
---
+++ p2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+int a = 2,147,483,649;
+int b = 4;
